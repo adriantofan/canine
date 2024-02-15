4fce00eb2d53421b353754e38cf142bb35287de4
Adrian <adrian@tofan.co>
2024-02-15 16:39:43 +0100

    [sync4] updated seed script

--- back/seed.py
+++ back/seed.py
@@ -5,11 +5,53 @@ import json
 base_url = "http://localhost:8080"
 
 
-# Function to create a user
-def create_user(messaging_address):
+def create_workspace(name, owner_email, owner_password):
     headers = {"Content-Type": "application/json"}
-    data = {"messaging_address": messaging_address}
-    response = requests.post(f"{base_url}/users", headers=headers, json=data)
+    data = {
+        "name": name,
+        "messaging_address": owner_email,
+        "password": owner_password
+    }
+
+    response = requests.post(f"{base_url}/workspaces", headers=headers, json=data)
+    if response.status_code != 201:
+        raise Exception(f"Failed to create workspace {response.status_code}, {response.text}")
+    response_data = response.json()
+    workspace_id = response_data.get("workspace").get("id")
+    user_id = response_data.get("user").get("id")
+    return workspace_id, user_id
+
+
+def login(workspace_id, messaging_address, password):
+    headers = {"Content-Type": "application/json"}
+    data = {
+        "username": messaging_address,
+        "password": password,
+        "workspace_id": workspace_id,
+    }
+
+    response = requests.post(f"{base_url}/login", headers=headers, json=data)
+    if response.status_code != 200:
+        raise Exception(f"Failed to login {response.status_code}, {response.text}")
+    response_data = response.json()
+    token = response_data.get("token")
+    return token
+
+
+# Function to create a user
+def create_user(token, workspace_id, messaging_address, user_type, password):
+    assert user_type in ["internal", "external"]
+
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {token}",
+    }
+    data = {
+        "messaging_address": messaging_address,
+        "user_type": user_type,
+        "password": password,
+    }
+    response = requests.post(f"{base_url}/{workspace_id}/users", headers=headers, json=data)
     if response.status_code != 201:
         raise Exception(f"Failed to create user {response.status_code}, {response.text}")
     response_data = response.json()
@@ -18,10 +60,13 @@ def create_user(messaging_address):
 
 
 # Function to create a conversation
-def create_conversation(recipient_phone):
-    headers = {"Content-Type": "application/json"}
+def create_conversation(token, workspace_id, recipient_phone):
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {token}",
+    }
     data = {"recipient_messaging_address": recipient_phone}
-    response = requests.post(f"{base_url}/conversations", headers=headers, json=data)
+    response = requests.post(f"{base_url}/{workspace_id}/conversations", headers=headers, json=data)
     if response.status_code != 201:
         raise Exception(f"Failed to create conversation {response.status_code}, {response.text}")
     response_data = response.json()
@@ -30,14 +75,15 @@ def create_conversation(recipient_phone):
 
 
 # Function to send a message
-def send_message(sender_id, conversation_id, message):
-    headers = {"Content-Type": "application/json"}
+def send_message(token, workspace_id, conversation_id, message):
+    headers = {
+        "Content-Type": "application/json",
+        "Authorization": f"Bearer {token}", }
     data = {
-            "sender_id": sender_id,
-            "message": message,
-            }
+        "message": message,
+    }
 
-    response = requests.post(f"{base_url}/conversations/{conversation_id}/messages", headers=headers,
+    response = requests.post(f"{base_url}/{workspace_id}/conversations/{conversation_id}/messages", headers=headers,
                              json=data)
     if response.status_code != 201:
         raise Exception(f"Failed to send message {response.status_code}, {response.text}")
@@ -51,18 +97,37 @@ def get_messages(user_id, conversation_id):
     return messages
 
 
+emma_email, emma_password = "emma@example.com", "emma forever"
+canine_id, emma_id = create_workspace("canine", emma_email, emma_password)
+print(f"created emma {emma_id} owning workspace {canine_id}")
+
+emma_token = login(canine_id, emma_email, emma_password)
+print("emma logged in")
+
 # Create a user
-user1_id = create_user("1234567890")
-print("created user", user1_id)
+ava_email, ava_password = "ava@example.com", "ava forever"
+ava_id = create_user(emma_token, canine_id, ava_email, "internal", ava_password)
+print("created ava", ava_id)
 
-# Create a conversation
-conv1_id = create_conversation("1234567890")
-print("created conversation", conv1_id)
+kitty_email = "kitty@example.com"
+kitty_id = create_user(emma_token, canine_id, kitty_email, "external", "kitty forever")
+print("created ava", kitty_id)
 
-# Send messages in a loop
-for i in range(1, 21):
-    send_message(user1_id, conv1_id, str(i))
+whiskers_email = "whiskers@example.com"
+whiskers_id = create_user(emma_token, canine_id, whiskers_email, "external", "whiskers forever")
+print("created whiskers", whiskers_id)
 
-# Retrieve messages
-messages = get_messages(user1_id, conv1_id)
-print(messages)
+# Login as ava
+ava_token = login(canine_id, ava_email, ava_password)
+print("ava logged in")
+
+withKitty = create_conversation(ava_token, canine_id, kitty_email)
+print("created conversation with kitty", withKitty)
+
+withWhiskers = create_conversation(emma_token, canine_id, whiskers_email)
+print("created conversation with whiskers", withWhiskers)
+
+send_message(emma_token, canine_id, withKitty, "hello kitty, here emma")
+print("to kitty: hello kitty, here emma")
+send_message(ava_token, canine_id, withKitty, "hello kitty, here ava")
+print("to kitty: hello kitty, here ava")
