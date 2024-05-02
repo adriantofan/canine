429898f709eada70d5f98266fc1c02f8d6a7f634
Adrian <adrian@tofan.co>
2024-05-02 18:08:46 +0200

    removed elm fe

--- fe/js/app.js
+++
@@ -1,9 +0,0 @@
-import {Elm} from '../src/Main.elm';
-import {setupSSE} from "./sse";
-
-app = Elm.Main.init({ node: document.getElementById('canine-app') });
-
-
-
-// Initialize the SSE connection
-setupSSE();
--- fe/src/Home.elm
+++
@@ -1,8 +0,0 @@
-module Home exposing (view)
-
-import Html exposing (..)
-
-
-view : Html msg
-view =
-    div [] [ text "Welcome to canine, your friendly AI assisted communication companion" ]
