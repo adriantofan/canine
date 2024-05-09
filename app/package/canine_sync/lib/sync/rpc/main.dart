b616e9b5b12f956d6eda5f443c6d5775cc27c795
Adrian <adrian@tofan.co>
2024-05-09 10:36:03 +0200

    bootstrap web

---
+++ app/package/canine_sync/lib/sync/rpc/main.dart
@@ -0,0 +1,78 @@
+import 'dart:isolate';
+
+import 'package:logging/logging.dart';
+
+import '../../api/main.dart';
+import '../../cache/in_memory_cache.dart';
+import '../../secure_storage/secure_storage_skeleton.dart';
+import '../../secure_storage/secure_storages_stub.dart';
+import '../sync.dart';
+import '../sync_service.dart';
+import './msg.dart';
+import './sync_skeleton.dart';
+import './sync_stub.dart';
+
+Future<Sync> start() async {
+  var receivePortStorage = ReceivePort();
+
+  // Needs to happen on the main isolate
+  await SecureStorageSkeleton().listen(receivePortStorage);
+
+  var receivePort = ReceivePort();
+
+  await Isolate.spawn(_runner, receivePort.sendPort);
+  final [sendPort, stubSendPort] = await receivePort.first as List<SendPort>;
+  stubSendPort.send(receivePortStorage.sendPort);
+
+  SyncStub stub = SyncStub(sendPort);
+  return stub;
+}
+
+_runner(SendPort sendPort) async {
+  Logger.root.level = Level.ALL; // defaults to Level.INFO
+  Logger.root.onRecord.listen((record) {
+    if (record.error != null || record.stackTrace != null) {
+      print(
+          '${record.level.name}: ${record.time}: ${record.loggerName}: ❗️${record.message} ${record.error} ${record.stackTrace}');
+    } else {
+      print(
+          '${record.level.name}: ${record.time}: ${record.loggerName}: ${record.message}');
+    }
+  });
+  final logger = Logger('runner');
+
+  ReceivePort receivePort = ReceivePort();
+  final tmpReceivePort = ReceivePort();
+
+  sendPort.send([receivePort.sendPort, tmpReceivePort.sendPort]);
+
+  SendPort sendPortStorage = await tmpReceivePort.first as SendPort;
+  SecureStoragesStub stub = SecureStoragesStub(sendPortStorage);
+
+  final cache = InMemoryCache();
+  APIClient apiClient = APIClient(stub);
+  await apiClient.init(); // loads credentials from disk
+
+  final syncService = SyncService(cache, apiClient);
+
+  // Communicates with SyncStub and delegates actual work to SyncService
+  final syncSkeleton = SyncSkeleton(syncService);
+
+  final cancelOnMsg = receivePort.listen((data) {
+    // even if this is running async with respect to _work, it is still
+    // running in the same isolate, so onMsg and onUpdate are not running
+    // concurrently with each other (they are synchronized by the event loop)
+    try {
+      final msg = data as Msg;
+      syncSkeleton.onMsg(msg);
+    } catch (e) {
+      logger.severe('Error in onMsg', e);
+    }
+  });
+  try {
+    await syncService.websocketListen();
+  } finally {
+    await cancelOnMsg.cancel();
+  }
+  logger.info('SyncSkeleton terminated');
+}
---
+++ app/package/canine_sync/lib/sync/web/main.dart
@@ -0,0 +1,33 @@
+import 'package:logging/logging.dart';
+
+import '../../api/api_client.dart';
+import '../../api/credential_set.dart';
+import '../../cache/in_memory_cache.dart';
+import '../../secure_storage/secure_storage.dart';
+import '../sync.dart';
+import '../sync_service.dart';
+
+class DummySecureStorage implements SecureStorage {
+  final Logger _logger = Logger('DummySecureStorage');
+  @override
+  Future<CredentialSet?> getCredentials() {
+    _logger.warning('DummySecureStorage.getCredentials not implemented on web');
+    return Future.value(null);
+  }
+
+  @override
+  Future<void> setCredentials(CredentialSet? credential) {
+    _logger.warning('DummySecureStorage.setCredentials not implemented on web');
+    return Future.value(null);
+  }
+}
+
+Future<Sync> start() async {
+  final cache = InMemoryCache();
+  APIClient apiClient = APIClient(DummySecureStorage());
+  await apiClient.init(); // loads credentials from disk
+
+  final syncService = SyncService(cache, apiClient);
+  syncService.websocketListen();
+  return syncService;
+}
