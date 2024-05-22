e6713087713025c1971857d0fc5e36a975110124
Adrian <adrian@tofan.co>
2024-05-22 17:01:39 +0200

    configured firebase

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,10 +1,12 @@
 import 'package:app/repository/repository.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:logging/logging.dart';
 
 import 'app/app.dart';
 import 'app/routes/routes.dart';
 import 'config.dart';
+import 'firebase_options.dart';
 
 void main() async {
   Logger.root.level = Level.ALL; // defaults to Level.INFO
@@ -20,6 +22,9 @@ void main() async {
   // usePathUrlStrategy();
   WidgetsFlutterBinding.ensureInitialized();
   await Config().loadConfig();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   final sync = await start(Config().apiBase, Config().wsBase);
   final syncRepository = SyncRepository(sync);
   final authRepository = AuthRepository(
